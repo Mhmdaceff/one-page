760fce684fbc14e2b2f1fa69255c5f5551eb14c4
Mhmdaceff <131608239+Mhmdaceff@users.noreply.github.com>
2023-10-31 23:45:53 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$("#app").click(function () {
+    $(".aps").css("display", "block")
+    $(".a1").css("display","none")
+})
+
+$("#card").click(function () {
+    $(".car").css("display", "block")
+    $(".c1").css("display","none")
+})
+$("#web").click(function () {
+    $(".web").css("display", "block")
+    $(".w1").css("display","none")
+})
+$("#all").click(function () {
+    $(".web").css("display", "block")
+    $(".w1").css("display","block")
+})
+
